86fbc5af06ee33f3380ee658d84c42a0cdfd742d
Sri-Ram-Saran <saran.sridhara@gmail.com>
2024-10-25 13:05:48 +0530

    Initial commit -added the tic tac toe files

---
+++ script2.js
@@ -0,0 +1,44 @@
+let current_player ="X";
+let tic=Array(9).fill(null);
+// console.log(tic);
+
+
+function winnerCheck(){
+    if(
+        (tic[0] !==null &&  tic[0]==tic[1] && tic[1]==tic[2])||
+        (tic[3] !==null && tic[3]==tic[4] && tic[4]==tic[5])||
+        (tic[6] !==null && tic[6]==tic[7] && tic[7]==tic[8])||
+        (tic[0] !==null && tic[0]==tic[3] && tic[3]==tic[6])||
+        (tic[1] !==null && tic[1]==tic[4] && tic[4]==tic[7])||
+        (tic[2] !==null && tic[2]==tic[5] && tic[5]==tic[8])||
+        (tic[0] !==null && tic[0]==tic[4] && tic[4]==tic[8])||
+        (tic[2] !==null && tic[2]==tic[4] && tic[4]==tic[6])  
+    ){
+        //document.write('Winner is ',current_player);
+        document.write(`winner is ${current_player}`);
+        return;
+    }
+    if(!tic.some((e)=>e===null)){
+        document.write('!!!!!!!!DRAW.......MATCH!!!!!!!!')
+        return;
+    }
+
+}
+// function disp(ell){
+//     const d=document.getElementById("Winner");
+//     d.innerHTML=`The Winner of our match is ${ell}`
+//     return ;
+// }
+
+function handleClick(el){
+    // console.log(el);
+    const id=Number(el.id);
+    if(tic[id]!==null) return;
+    
+    tic[id]=current_player;
+    el.innerText=current_player;
+    winnerCheck();
+    current_player=current_player==="X"? "O":"X";
+    //console.log(tic);
+    
+}
